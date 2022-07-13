ae4b692e0940635dc1da12be4024e702bd7ffe4a
Manasvi <manasvinarayan2@gmail.com>
2022-07-13 23:06:21 +0530

    some changes

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,20 @@
-from jinja2 import Environment, FileSystemLoader
+print('importing libraries...')
 
+from jinja2 import Environment, FileSystemLoader
+import pandas as pd
+
+print('processing...')
 env = Environment(loader=FileSystemLoader('templates'))
 
 template = env.get_template('template.html')
 
+df = pd.read_csv('Responses (1).csv')
+
+print('creating dashboard ...')
+board = template.render(title='Covid-19 Survey',
+                        df = df)
+
+
+with open('templates/index.html', 'w') as f:
+    f.write(board)
+print('done!')
